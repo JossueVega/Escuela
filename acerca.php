6c3d6eec3bb0a60946c09e9bdcdef0593358e371
Juan Manuel Hernandez Martinez <juan.hernandezm@outlook.com>
2020-11-17 18:54:00 -0800

    update data

---
+++ acerca.php
@@ -0,0 +1,3 @@
+<?php include 'layouts/head.php'; ?>
+<!-- todo lo que este aqui -->
+<?php include 'layouts/footer.php'; ?>
---
+++ index.php
@@ -0,0 +1,18 @@
+
+<?php include 'layouts/head.php';  ?>
+<!-- Esta parte es codigo -->
+<div class="jumbotron">
+  <h1 class="display-4">Bienvenidos a University</h1>
+  <p class="lead">Educando con Amor</p>
+  <hr class="my-4">
+  <p>Superate Hoy</p>
+  <?php echo $datos ?>
+  <p class="lead">
+    <a class="btn btn-primary btn-sm" href="#" role="button">Mas..</a>
+  </p>
+</div>
+
+
+<!-- esta parte es codigo -->
+
+<?php include'layouts/footer.php'; ?>
